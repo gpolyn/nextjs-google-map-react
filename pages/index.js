8363233c8b839b059857361e626eb92c291a4c31
Gallagher Polyn <gallagher.polyn@gmail.com>
2018-05-10 12:25:35 -0400

    simplify branch, add readme

--- pages/index.js
+++ pages/index.js
@@ -3,44 +3,25 @@ import Head from 'next/head'
 import GoogleMapReact from 'google-map-react';
 import { locations } from '../locations';
 import { GMAPS_API_KEY } from '../config';
+import MyGreatPlaceWithHover from '../components/my_great_place_with_hover';
+import { K_SIZE } from '../components/my_great_place_with_hover_styles';
 
 const mapCenter = {lat: 38.91131141655464, lng: -77.04375138092037};
-const markerCredit = 'Map Markers by Viktor Vorobyev from the Noun Project'
-const MARKER_SIZE = 34;
-const placeStyle = {
-  position: 'absolute',
-	transform: 'translate(-50%, -50%)'
-}
-// const markerSrc = '../static/marker_2.svg';
-const markerSrc = '../static/blah_050916.png';
-
-// const markerStyle = { display: 'inline-block', height: MARKER_SIZE, width: MARKER_SIZE, cursor: 'pointer', transform: 'translate(-25%,-100%)'}
-const markerStyle = { ...placeStyle, display: 'inline-block', cursor: 'pointer' }
 const marker = ({id}) => ( <div key={id} style={markerStyle}>
 														{id}
 													</div> )
-// const AnyReactComponent = ({ id }) => (
-//     <img className='marker-svg' src={markerSrc} fill={'#666'} style={{ ...markerStyle, width: MARKER_SIZE, height: MARKER_SIZE}} />
-// )
 
-const AnyReactComponent = ({ id }) => (
-    <img className='marker-svg' src={markerSrc} fill={'#666'} style={{ ...markerStyle }} />
-)
 const markers = ( locations, handler ) => {
   return locations.map(location => (
-    <AnyReactComponent
+    <MyGreatPlaceWithHover
       text={location.id}
-      id={location.id}
-      key={location.id}
       lat={location.lat}
       lng={location.lng}
     />
   ))
 }
 
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class SimpleMap extends Component {
+class SimpleHoverMap extends Component {
   static defaultProps = {
     center: mapCenter,
     zoom: 16
@@ -58,10 +39,10 @@ class SimpleMap extends Component {
           bootstrapURLKeys={{ key: GMAPS_API_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
+          hoverDistance={K_SIZE / 2}
         >
 					{markers(locations)}
         </GoogleMapReact>
-				<span style={{transform: 'translateZ(1px)', position: 'absolute', top: 13, left: 5, fontSize: 13, color: 'rgb(95, 158, 160)'}}> {markerCredit} </span>
 				<style jsx global>{`
 					body { 
 						margin: 0;
@@ -73,4 +54,4 @@ class SimpleMap extends Component {
   }
 }
 
-export default SimpleMap;
+export default SimpleHoverMap;
---
+++ readme.md
@@ -0,0 +1,18 @@
+<p>
+```bash
+node -v # >= 8 
+npm install 
+```
+</p>
+
+<p>
+Then just run `npm run dev` and go to `http://localhost:3000`. To use another port, you can run `npm run dev -- -p <your port here>`.
+</p>
+
+<p>
+After dev, try
+```bash
+next build
+next start
+```
+</p>

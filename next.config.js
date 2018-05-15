721b6a9620e3fa523e3649c55b6fc6f15dd83ed0
Gallagher Polyn <gallagher.polyn@gmail.com>
2018-05-15 12:38:25 -0400

    transferred material from github.com/zeit/next.js/tree/canary/examples/using-inferno

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpack: function (config, { dev }) {
+    // For the development version, we'll use React.
+    // Because, it support react hot loading and so on.
+    if (dev) {
+      return config
+    }
+
+    config.resolve.alias = {
+      'react': 'inferno-compat',
+      'react-dom': 'inferno-compat'
+    }
+    return config
+  }
+}
